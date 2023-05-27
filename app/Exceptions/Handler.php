7a15166f04ede9a4056c6f899c6256f3b15e2abb
Kim@PC <kimdeguzman20@yahoo.com>
2023-05-27 10:16:07 +0800

    Scaffold Throwable Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,22 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ResponsesJson;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+use Laravel\Sanctum\Exceptions\MissingAbilityException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ResponsesJson;
+
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,5 +37,58 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        Log::error(print_r([
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'message' => $e->getMessage()
+        ], true));
+
+        if (method_exists($e, 'render') && $response = $e->render($request)) {
+            return Router::toResponse($request, $response);
+        } elseif ($e instanceof Responsable) {
+            return $e->toResponse($request);
+        }
+
+        if($e instanceof MissingAbilityException){
+            return $this->invalidAbility($request, $e);
+        }
+
+        $e = $this->prepareException($this->mapException($e));
+
+        if ($response = $this->renderViaCallbacks($request, $e)) {
+            return $response;
+        }
+
+        return match (true) {
+            $e instanceof HttpResponseException => $e->getResponse(),
+            $e instanceof AuthenticationException => $this->unauthenticated($request, $e),
+            $e instanceof ValidationException => $this->convertValidationExceptionToResponse($e, $request),
+            default => $this->renderExceptionResponse($request, $e),
+        };
+    }
+
+    protected function renderExceptionResponse($request, Throwable $e)
+    {
+        return $this->shouldReturnJson($request, $e)
+            ? $this->serverErrorResponse()
+            : $this->prepareResponse($request, $e);
+    }
+
+    protected function invalidAbility($request, AuthorizationException $exception)
+    {
+        return $this->shouldReturnJson($request, $exception)
+            ? $this->validationErrorResponse()
+            : $this->renderExceptionResponse($request, $exception);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->shouldReturnJson($request, $exception)
+            ? $this->unauthorizedResponse()
+            : redirect()->guest($exception->redirectTo() ?? route('login'));
     }
 }
